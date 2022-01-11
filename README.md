7748307eaba47f0195fab3172fd240caab8d1f3e
Samuel Leal <samuelmarquessousaleal@gmail.com>
2022-01-11 16:55:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Calculadora-desafio-front-end
+ Uma calculadora que deve ter 3 temas e ser funcional
---
+++ script/calculador.js
@@ -0,0 +1,70 @@
+
+const tela = document.querySelector('.texto-tela')
+const numeros = document.querySelectorAll('[id*=num]')
+const operadores = document.querySelectorAll('[id*=op]')
+
+let novoNumero = true
+let operador
+let numeroAnterior
+
+const operacaoPendente = () => operador !== undefined
+
+const calcular = () => {
+    if (operacaoPendente()){    
+        const numeroNovo = parseFloat(tela.textContent)
+        novoNumero = true
+        if (operador == 'x'){
+            operador = '*'
+        }
+        const resultado = eval (`${numeroAnterior}${operador}${numeroNovo}`)
+        atualizarTela(resultado)
+    }
+}
+
+const atualizarTela =  (texto) => {
+    if(novoNumero){
+        tela.textContent = texto;
+        novoNumero = false
+    } else{
+        tela.textContent += texto;
+    }
+}
+
+const digitarNumero = (evento) => atualizarTela(evento.target.textContent)
+
+numeros.forEach((numero) => numero.addEventListener('click', digitarNumero))
+
+const  selecionarOperador = (evento) => {
+    if(!novoNumero) { 
+        calcular()  
+        novoNumero = true
+        operador = evento.target.textContent
+        numeroAnterior  = parseFloat(tela.textContent)
+    } 
+}
+operadores.forEach((operador) => operador.addEventListener('click', selecionarOperador))
+
+const acionarIgual = () => {
+    calcular();
+    operador = undefined
+}
+document.getElementById('igual').addEventListener('click', acionarIgual)
+
+const removerUltimoNumero = () => tela.textContent = tela.textContent.slice(0, -1)
+document.getElementById('del').addEventListener('click', removerUltimoNumero)
+
+const resetarCalculo = () => {
+    tela.textContent = ''
+    operador = undefined
+    novoNumero = true
+    numeroAnterior = undefined
+}
+document.getElementById('reset').addEventListener('click', resetarCalculo)
+
+const existeDecimal = () => tela.textContent.indexOf('.') !== -1
+const inserirDecimal = () => {
+    if(!existeDecimal()){
+        atualizarTela('.')
+    }
+}
+document.getElementById('decimal').addEventListener('click', inserirDecimal)
